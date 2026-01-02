4b2378be4a752276c39b7ba103e2d8a02d8a9dd9
Stan Powell <git@stanpowell.com>
2026-01-02 13:18:03 -0600

    updated fle_update

--- fle_update_process.php
+++ fle_update_process.php
@@ -5,25 +5,25 @@ $today = date('Y-m-d');
 $PO = $_POST['PO'];
 $Invoice = $_POST['Invoice'];
 $Ticket = $_POST['Ticket'];
-$success = 'wgutierrez@fulllineexhaust.com,bill.kalt@car-mart.com,arlie.bartholomew@car-mart.com,shelby.hale@car-mart.com';
-$subject = 'PO Confirmation: ';
-$subject .= $PO;
-$message = 'PO recieved and invoiced. Shipping to follow.';
+//$success = 'wgutierrez@fulllineexhaust.com,bill.kalt@car-mart.com,arlie.bartholomew@car-mart.com,shelby.hale@car-mart.com';
+//$subject = 'PO Confirmation: ';
+//$subject .= $PO;
+//$message = 'PO recieved and invoiced. Shipping to follow.';
 //$notify = 'wgutierrez@fulllineexhaust.com';
     
 
-$sql = "UPDATE car_mart_ticket_new SET PONumber = '$PO', InvoiceNumber = '$Invoice' WHERE Ticket = '$Ticket'";
+$sql = "UPDATE car_mart_ticket_new SET PONumber = '$PO', InvoiceNumber = '$Invoice',date_updated = '$today',`status` = 'invoiced' WHERE Ticket = '$Ticket'";
 $result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
 
-$content = 'PO Invoiced.<br><br>';
+$content = 'PO Invoiced.<br><br>Ticket Updated.<br><br>';
 
 
-$query = "UPDATE car_mart_ticket_new SET date_updated = '$today', status = 'invoiced' WHERE Ticket = '$Ticket'";
-$res = mysqli_query($conn,$query) or die(mysqli_error($conn));
+//$query = "UPDATE car_mart_ticket_new SET date_updated = '$today', `status` = 'invoiced' WHERE Ticket = '$Ticket'";
+//$res = mysqli_query($conn,$query) or die(mysqli_error($conn));
 
 
 
-$content .= 'Ticket Updated.<br><br>';
+//$content .= 'Ticket Updated.<br><br>';
 
 /*mail($success,$subject,$message);
 
