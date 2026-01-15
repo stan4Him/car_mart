4726b9c3d71d39dec0c2552e39e6d295230274a4
Stan Powell <git@stanpowell.com>
2026-01-15 13:21:01 -0600

    updated old quotes mover

--- move_older.php
+++ move_older.php
@@ -3,15 +3,15 @@ $title = 'Move Older';
 // Config
 include 'db_conn.php';
 
-$query = "SELECT * FROM cm_ticket WHERE status = 'quoted' AND DATE(date_added) <= DATE_SUB(CURDATE(), INTERVAL 30 DAY)";
+$query = "SELECT * FROM car_mart_ticket_new WHERE status = 'quoted' AND DATE(date_added) <= DATE_SUB(CURDATE(), INTERVAL 30 DAY)";
 $result = mysqli_query($conn,$query) or die(mysqli_error($conn));
 
 while ($row = mysqli_fetch_array($result)) {
     $Ticket = $row['0'];
 
-    $q =  mysqli_query($conn,"INSERT INTO cm_iqs_older SELECT * FROM cm_ticket WHERE Ticket ='$Ticket'") or die(mysqli_error($conn));
-//    $q2 =  mysqli_query($conn,"DELETE FROM cm_parts WHERE Ticket ='$Ticket'") or die(mysqli_error($conn));
-    $q3 =  mysqli_query($conn,"DELETE FROM cm_ticket_older WHERE Ticket ='$Ticket'") or die(mysqli_error($conn));
+    $q =  mysqli_query($conn,"INSERT INTO car_mart_ticket_aged SELECT * FROM car_mart_ticket_new WHERE Ticket ='$Ticket'") or die(mysqli_error($conn));
+
+    $q3 =  mysqli_query($conn,"DELETE FROM car_mart_ticket_new WHERE Ticket ='$Ticket'") or die(mysqli_error($conn));
 }
 
 
